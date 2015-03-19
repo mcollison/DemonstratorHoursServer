f90502483ff0360b96cf19101e8940a1ba5e552a
Matt Collison <m.g.collison@gmail.com>
2015-03-19 15:46:58 +0000

    Email test now working.

--- src/main/java/emailtests/SendEmail.java
+++ src/main/java/emailtests/SendEmail.java
@@ -15,16 +15,7 @@ import javax.mail.internet.*;
  */
 public class SendEmail {
 
-    public static void mainmethodstub(String[] args) {
-//        System.out.println(System.getProperties());
-//        String from = "m.g.collison@gmail.com";
-//        String to = "matthew.collison@ncl.ac.uk";
-//        String host = "smtp.gmail.com";
-//        Properties properties = new Properties();
-//        properties.setProperty("mail.smtp.host", host);
-//        properties.setProperty("mail.smtp.port", "465");
-//        properties.setProperty("mail.smtp.password", "465");
-//        Session session = Session.getDefaultInstance(properties);
+    public static void main(String[] args) {
 
         final String username = "nmc91";
         final String password = "";
@@ -32,27 +23,23 @@ public class SendEmail {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "SMTPAUTH.NCL.AC.UK");
-        props.put("mail.smtp.port", "465");
-        System.out.println("checkpoint 1");
+        props.put("mail.smtp.host", "smtpauth.ncl.ac.uk");
+        props.put("mail.smtp.port", "587");
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(username, password);
                     }
                 });
-        System.out.println("checkpoint 2");
 
         try {
 
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("matthew.collison@ncl.ac.uk"));
             message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("matthew.collison@ncl.ac.uk"));
+                    InternetAddress.parse("m.g.collison@ncl.ac.uk"));
             message.setSubject("Testing Subject");
-            message.setText("Dear Mail Crawler,"
-                    + "\n\n No spam to my email, please!");
-        System.out.println("checkpoint 3");
+            message.setText("Test message");
 
             Transport.send(message);
 
@@ -62,36 +49,5 @@ public class SendEmail {
             throw new RuntimeException(e);
         }
 
-//        Session session = Session.getInstance(props,
-//                new javax.mail.Authenticator() {
-//                    protected PasswordAuthentication getPasswordAuthentication() {
-//                        return new PasswordAuthentication(username, password);
-//                    }
-//                });
-//
-//        try {
-//            // Create a default MimeMessage object.
-//            MimeMessage message = new MimeMessage(session);
-//
-//            // Set From: header field of the header.
-//            message.setFrom(new InternetAddress(from));
-//
-//            // Set To: header field of the header.
-//            message.addRecipient(Message.RecipientType.TO,
-//                    new InternetAddress(to));
-//
-//            // Set Subject: header field
-//            message.setSubject("This is the Subject Line!");
-//
-//            // Now set the actual message
-//            message.setText("This is actual message");
-//
-//            // Send message
-//            Transport.send(message);
-//            System.out.println("Sent message successfully....");
-//        } catch (MessagingException mex) {
-//            mex.printStackTrace();
-//        }
-//
     }
 }
